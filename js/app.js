0ef6ca823fee016d9d84bdbfd64293a1939d0c9a
iinaamasum <iinaamasum@gmail.com>
2022-02-17 00:26:48 +0600

    validation and update balance

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 function readInput(id) {
   const intput = document.getElementById(id);
   const inputValue = parseFloat(intput.value);
-  intput.value = "";
-  return inputValue;
+  if (inputValue === Number) {
+    intput.value = "";
+    return inputValue;
+  } else {
+    intput.style.background = "red";
+    intput.value = "Please Enter a Number"
+  }
 }
 
 /* catching input from input feild */
 document.getElementById("calculate-btn").addEventListener("click", function () {
-  const inputValOne = readInput("input-one");
-  const inputValTwo = readInput("input-two");
-  const inputValThree = readInput("input-three");
-  const inputValFour = readInput("input-four");
-  const inputValFive = readInput("input-five");
+  const income = readInput("input-one");
+  const food = readInput("input-two");
+  const rent = readInput("input-three");
+  const clothes = readInput("input-four");
+  const savingPercent = readInput("input-five");
+
+  const totalExpenses = food + rent + clothes;
+  const currentBalance = income - totalExpenses;
+
+  document.getElementById("total-exp").innerText = totalExpenses;
+  document.getElementById("balance").innerText = currentBalance;
 });
