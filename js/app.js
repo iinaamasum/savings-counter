754e02c808725fe48effbbd8144c1f04a232bdc6
iinaamasum <iinaamasum@gmail.com>
2022-02-17 00:45:20 +0600

    percent calculation

--- js/app.js
+++ js/app.js
@@ -5,22 +5,29 @@ function readInput(id) {
     intput.value = "";
     return inputValue;
   } else {
-    intput.style.background = "red";
-    intput.value = "Please Enter a Number"
+    intput.placeholder = "It's not a number";
   }
 }
 
-/* catching input from input feild */
+function calcPercent(income, savingPercent) {
+  return (income * savingPercent) / 100;
+}
+/* catching input from expense and income feild */
 document.getElementById("calculate-btn").addEventListener("click", function () {
-  const income = readInput("input-one");
+  var income = readInput("input-one");
   const food = readInput("input-two");
   const rent = readInput("input-three");
   const clothes = readInput("input-four");
-  const savingPercent = readInput("input-five");
 
   const totalExpenses = food + rent + clothes;
   const currentBalance = income - totalExpenses;
 
   document.getElementById("total-exp").innerText = totalExpenses;
   document.getElementById("balance").innerText = currentBalance;
+});
+
+/* catching input from save feild */
+document.getElementById("save").addEventListener("click", function () {
+  const savingPercent = readInput("input-five");
+  const sacingAmount = calcPercent(savingPercent);
 });
